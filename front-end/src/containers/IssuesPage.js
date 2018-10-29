f381363cdcf2fa2f0b908fcb707747f4abdd4e0c
Anthony <Anthony@Anthonys-MacBook-Pro-3.local>
2018-10-29 00:32:47 -0600

    first commit of front-end app.

---
+++ front-end/src/containers/IssuesPage.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import Issue from '../components/Issue';
+import './IssuesPage.css';
+
+class IssuesPage extends Component {
+    
+    state = {
+      issues: []
+    }
+
+    componentDidMount() {
+      axios.get('https://api.github.com/repos/angular/angular/issues')
+      .then(response => {
+        this.setState({ issues: response.data});
+        console.log(response);
+      })
+    }
+
+    render () {
+
+        const issues = this.state.issues.map(
+          issue => { 
+            return <Issue 
+                  key={issue.number} 
+                  title={issue.title}
+                  body={issue.body}
+                  user={issue.user.login}
+                  assignee={issue.assignee}/>
+          }
+        );
+
+        return (
+            <div>
+                {issues}
+            </div>
+        );
+    }
+}
+
+export default IssuesPage;
