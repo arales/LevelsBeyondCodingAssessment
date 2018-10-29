18774560ae75c25cabea8ff97d14bb1bd6ca9f12
Anthony <Anthony@Anthonys-MacBook-Pro-3.local>
2018-10-28 21:22:18 -0600

    first push of 'front-end' app.

---
+++ front-end/angular-issues-viewer/src/components/Issue.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from './Issue.css';
+
+const = (props) => (
+  <div className={classes.Issue}>
+    <h1>ISSUE COMPONENT</h1>
+  </div>
+);
+
+export default function Issue;
